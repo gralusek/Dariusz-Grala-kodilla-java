1616af1aff09092b88a7f150f4acc4fcdee585ab
Dariusz Grala <gralusek@interia.pl>
2020-12-07 21:07:06 +0100

    6.6 Task

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/Library/BookLibrary.java
@@ -0,0 +1,33 @@
+package com.kodilla.testing.Library;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BookLibrary {
+
+    private LibraryDatabase libraryDatabase;
+
+    public BookLibrary(LibraryDatabase libraryDatabase) {
+        this.libraryDatabase = libraryDatabase;
+    }
+
+
+
+    public List<Book> listBooksWithCondition(String titleFragment) {
+        List<Book> bookList = new ArrayList<Book>();
+        if (titleFragment.length() < 3) return bookList;
+        List<Book> resultList = libraryDatabase
+                .listBooksWithCondition(titleFragment);
+        if (resultList.size() > 20) return bookList;
+        bookList = resultList;
+        return bookList;
+    }
+
+    public Map<LibraryUser,List<Book>> userBooksMap = new HashMap<>();
+
+    public List<Book> listBooksInHandsOf(LibraryUser libraryUser) {
+        return userBooksMap.get(libraryUser);
+    }
+}
